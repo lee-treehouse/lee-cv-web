99d0287c73b00a37dde50a3d5d66c4fe01d5dfe7
lee-treehouse <leentaylor@gmail.com>
2022-09-13 12:42:48 +1000

    update dependencies, fix broken test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<App />, { wrapper: BrowserRouter });
+  const headingElement = screen.getByText(/Lee Taylor/i);
+  expect(headingElement).toBeInTheDocument();
 });
